1160f0f6b6b3309410696e854edb905bf5081126
robloxxa <robloxxa@yandex.ru>
2023-02-03 15:03:51 +0300

    type safe event addhandle functions

--- banchoClient_events.go
+++ banchoClient_events.go
@@ -1,25 +1,49 @@
-package ircbanchogo
+package banchogo
 
-func (b *BanchoClient) OnConnect(callback func()) {
-	b.Event.on("connect", callback)
+func (b *BanchoClient) HandleConnect(handler func()) func() {
+	return b.AddHandler("Connect", handler)
 }
 
-func (b *BanchoClient) OnStateChanged(callback func(ConnectState)) {
-	b.Event.on("stateChanged", callback)
+func (b *BanchoClient) HandleConnectOnce(handler func()) func() {
+	return b.AddHandlerOnce("Connect", handler)
 }
 
-func (b *BanchoClient) OnError(callback func(error)) {
-	b.Event.on("error", callback)
+func (b *BanchoClient) HandleDisconnect(handler func(error)) func() {
+	return b.AddHandler("Disconnect", handler)
 }
 
-func (b *BanchoClient) OnRawMessage(callback func(string)) {
-	b.Event.on("rawMessage", callback)
+func (b *BanchoClient) HandleDisconnectOnce(handler func(error)) func() {
+	return b.AddHandlerOnce("Disconnect", handler)
 }
 
-func (b *BanchoClient) OnPrivateMessage(callback func(BanchoUser, string)) {
-	b.Event.on("privateMessage", callback)
+func (b *BanchoClient) HandleError(handler func(error)) func() {
+	return b.AddHandler("Error", handler)
 }
 
-func (b *BanchoClient) OnChannelMessage(callback func(BanchoUser, string)) {
-	b.Event.on("channelMessage", callback)
+func (b *BanchoClient) HandleErrorOnce(handler func(error)) func() {
+	return b.AddHandlerOnce("Error", handler)
+}
+
+func (b *BanchoClient) HandleRawMessage(handler func([]string)) func() {
+	return b.AddHandler("RawMessage", handler)
+}
+
+func (b *BanchoClient) HandleRawMessageOnce(handler func([]string)) func() {
+	return b.AddHandlerOnce("RawMessage", handler)
+}
+
+func (b *BanchoClient) HandlePrivateMessage(handler func(*PrivateMessage)) func() {
+	return b.AddHandler("PrivateMessage", handler)
+}
+
+func (b *BanchoClient) HandlePrivateMessageOnce(handler func(*PrivateMessage)) func() {
+	return b.AddHandlerOnce("PrivateMessage", handler)
+}
+
+func (b *BanchoClient) HandleMessage(handler func(BanchoMessage)) func() {
+	return b.AddHandler("Message", handler)
+}
+
+func (b *BanchoClient) HandleMessageOnce(handler func(BanchoMessage)) func() {
+	return b.AddHandlerOnce("Message", handler)
 }
