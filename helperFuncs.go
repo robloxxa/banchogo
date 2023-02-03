ad75ed71c9a71f9d1e5f28f7586faa968cbf38cb
robloxxa <robloxxa@yandex.ru>
2023-02-03 14:58:29 +0300

    truncatestring function

---
+++ helperFuncs.go
@@ -0,0 +1,15 @@
+package banchogo
+
+import "unicode/utf8"
+
+func TruncateString(str string, length int) string {
+	if length <= 0 {
+		return ""
+	}
+
+	if utf8.RuneCountInString(str) < length {
+		return str
+	}
+
+	return string([]rune(str)[:length])
+}
