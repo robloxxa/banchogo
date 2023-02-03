7ac2c3d9372e41299e4dd983edf731d3514ab933
robloxxa <robloxxa@yandex.ru>
2023-02-03 15:01:56 +0300

    Message sender interface implementation

--- banchoChannel.go
+++ banchoChannel.go
@@ -1,8 +1,38 @@
-package ircbanchogo
+package banchogo
+
+import (
+	"github.com/puzpuzpuz/xsync/v2"
+)
 
 type BanchoChannel struct {
-	Name    string
-	Topic   string
-	Joined  bool
-	Members map[string]BanchoChannelMember
+	client *BanchoClient
+
+	ChannelName string
+	Topic       string
+	Joined      bool
+	Members     *xsync.MapOf[string, *BanchoChannelMember]
+
+	joinSignal chan error
+}
+
+func NewBanchoChannel(b *BanchoClient, name string) *BanchoChannel {
+	return &BanchoChannel{
+		client:      b,
+		ChannelName: name,
+		Topic:       "",
+		Joined:      false,
+		Members:     xsync.NewMapOf[*BanchoChannelMember](),
+	}
+}
+
+func (c *BanchoChannel) Name() string {
+	return c.ChannelName
+}
+
+func (c *BanchoChannel) SendMessage(message string) <-chan error {
+	return newOutgoingBanchoMessage(c.client, c, message).Send()
+}
+
+func (c *BanchoChannel) SendAction(message string) <-chan error {
+	return newOutgoingBanchoMessage(c.client, c, "ACTION "+message).Send()
 }
