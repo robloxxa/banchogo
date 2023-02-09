7679af3fefe3fb0d3f4cf62c70efbac7309b00d2
robloxxa <robloxxa@yandex.ru>
2023-02-09 04:26:35 +0300

    added constructor

--- channelMember.go
+++ channelMember.go
@@ -1,5 +1,7 @@
 package banchogo
 
+import "strings"
+
 type ChannelMemberMode string
 
 const (
@@ -7,8 +9,36 @@ const (
 	IRCModerator                   = "o"
 )
 
+func (c ChannelMemberMode) ToSymbol() string {
+	switch c {
+	case "v":
+		return "+"
+	case "o":
+		return "@"
+	default:
+		return ""
+	}
+}
+
 type ChannelMember struct {
 	Channel *Channel
 	User    *User
 	Mode    ChannelMemberMode
+}
+
+func newChannelMember(b *Client, channel *Channel, username string) (c *ChannelMember) {
+	c = &ChannelMember{
+		Channel: channel,
+	}
+	if strings.Index(username, "@") == 0 {
+		c.Mode = IRCModerator
+		username = username[1:]
+	} else if strings.Index(username, "+") == 0 {
+		c.Mode = IRCUser
+	} else {
+		c.Mode = ""
+	}
+	c.User = b.GetUser(username)
+	return
+
 }
